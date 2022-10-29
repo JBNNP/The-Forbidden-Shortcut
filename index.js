04c14e6fb484cd6d2370139ddf75b28c7f16582d
John Brian. Capate <93503234+JBNNP@users.noreply.github.com>
2022-10-30 06:21:39 +0800

    add comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let x = 1;
 
-const hotdog = new Promise( (res, rej) => 
-x === 1 ? res("Hooray!") : rej("Oh no"));
+const hotdog = new Promise( (res, rej) =>  //Promises
+x === 1 ? res("Hooray!") : rej("Oh no")); //Conditional Ternary Operators
 
-hotdog.then( (message) => console.log(message) )
+//Using advance arrow functions
+hotdog.then( (message) => console.log(message) ) 
 .catch( (message) => console.log(message) )
 .finally( () => console.log("execution completed"));
 
